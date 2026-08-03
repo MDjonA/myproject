b0a59afde42fcb44596b05b7c8240af9b5ec857d
MDjonA <57110449+MDjonA@users.noreply.github.com>
2026-08-03 07:49:48 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -133,6 +133,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
